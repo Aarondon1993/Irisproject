1280aaa6dda100102a830fbcb860384eaee77ebe
Aarondon1993 <aarondonnelly@live.ie>
2020-04-20 17:21:21 +0100

    Created python program

---
+++ analysis.py
@@ -0,0 +1,5 @@
+#Aaron Donnelly
+# This program is written as a requirement for the Programming and Scripting module in the Data Analytics course.
+
+print (" Below is a summary of each variabe in the Fischer iris data set")
+
