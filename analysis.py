d5c7cdb99a78e8b535d7000a85b46f5e07070bc4
Aarondon1993 <aarondonnelly@live.ie>
2020-04-25 16:21:13 +0100

    Updated analysis.py with summary of data file

--- analysis.py
+++ analysis.py
@@ -3,3 +3,10 @@
 import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as pyplot
+# The above libraries will be used to read the data in the .csv file, summarise the variables
+# and then produce both the histograms and scatter plots.
+
+df = pd.read_csv("IrisDataset.csv")
+# Using pandas the IrisDataset can be read.
+print (df.describe(include= 'all'))
+
