ed158c856d4864df86257a61f9473ca6f3cd035b
Aarondon1993 <aarondonnelly@live.ie>
2020-04-25 14:10:32 +0100

    Added new paragraph to readme a brief background of python

--- analysis.py
+++ analysis.py
@@ -1,5 +1,5 @@
 #Aaron Donnelly
 # This program is written as a requirement for the Programming and Scripting module in the Data Analytics course.
-
-print (" Below is a summary of each variabe in the Fischer iris data set")
+import numpy as np
+import pandas as pd 
 
