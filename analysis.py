51837a14a75a42f18e0148b8fc845e383a6089ef
Aarondon1993 <aarondonnelly@live.ie>
2020-04-26 11:06:52 +0100

    Added comments on code

--- analysis.py
+++ analysis.py
@@ -7,9 +7,15 @@ import matplotlib.pyplot as pyplot
 # and then produce both the histograms and scatter plots.
 
 df = pd.read_csv("IrisDataset.csv")
+# Using the .read_csv function of pandas the dataset can be read.
 desc = (df.describe())
+# in order to generate a summary of the dataset contents the df.describe funtion is used.
 f = open ('datasetsummary.txt','w')
-# Using pandas the IrisDataset can be read.
+# The variable 'f' is used to open the destination txt file in write mode.
+# Using write mode 'w' this will ensure that the code will overwrite the contents
+# of the .txt file each time to keep it tidy. 
+
 f.write (desc.to_string())
+# The df.describe must be converted into a string in order for the write funtion to work.
 
 
